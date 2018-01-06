231a86852fc2de06497c3f3bf2fea4549721f58f
rhapsody <richer6605@gmail.com>
2018-01-07 04:02:55 +0800

    Add rentTypeFilter to classify data feeds Added functions:     rentTypeFilter: classify data as different rent types Note:     rename firstFilter as rentTypeFilter

--- facebook_rental_extractor.py
+++ facebook_rental_extractor.py
@@ -33,4 +33,22 @@ def getAllGroupFeeds(group_id, access_token, since):
         res_json = res.json()
     return feeds
 
-def firstFilter():
+def rentTypeFilter(feeds):
+    feeds_filtered = {
+        "rent-out": [],
+        "rent": [],
+        "sublet": []
+    }
+    for feed in feeds:
+        message = feed['message']
+        message_bytes = message.encode('utf-8')
+        if (
+            b'\xe6\xb1\x82\xe7\xa7\x9f' in message_bytes or
+            b'\xe9\xa0\x90\xe7\xae\x97' in message_bytes
+           ):
+            feeds_filtered['rent'].append(feed)
+        elif b'\xe8\xbd\x89\xe7\xa7\x9f' in message_bytes:
+            feeds_filtered['sublet'].append(feed)
+        else:
+            feeds_filtered['rent-out'].append(feed)
+    return feeds_filtered
